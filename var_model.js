d0e860285d1b300941c563b422723723037644cb
Frank Blaauw <frank.blaauw@gmail.com>
2015-06-10 15:49:16 +0200

    Working version

--- var_model.js
+++ var_model.js
@@ -1,11 +1,20 @@
-var VarModel = function(var_coefficients, lags, number_of_variables, node_names) {
-    this.var_coefficients = var_coefficients;
-    this.lags = lags;
-    this.number_of_variables = number_of_variables;
+var VarModel = function(var_coefficients, node_names) {
+    this.lags = var_coefficients.length;
+    this.number_of_variables = node_names.length;
+
+    var concatted_var_coefficients = [];
+    for (var i = 0 ; i< this.lags ; i++ ) {
+        for (var j = 0; j < this.number_of_variables; j++) {
+            concatted_var_coefficients = concatted_var_coefficients.concat(transpose(var_coefficients[i]));
+        }
+    }
+
+    this.var_coefficients = transpose(concatted_var_coefficients);
+
     if (var_coefficients.length < 1 || var_coefficients[0].length < 1) throw "At least one parameter is needed in the VAR model";
-    this.var_coefficients = var_coefficients;
-    this.lags = lags;
-    this.number_of_variables = number_of_variables;
+
     this.node_names = node_names;
     this.number_of_exogen_variables = var_coefficients[0].length - this.lags * number_of_variables;
+    console.log('!!!!!!!!!!!');
+    console.log(this)
 };
