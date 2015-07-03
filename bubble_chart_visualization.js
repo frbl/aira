410042b6778349fb7fbeb836b966b2537166991e
Frank Blaauw <frank.blaauw@gmail.com>
2015-07-03 14:37:07 +0200

    WIP

--- bubble_chart_visualization.js
+++ bubble_chart_visualization.js
@@ -1,39 +1,63 @@
 var width = 600,
     height = 400,
     offset_y = 100;
-offset_x = 50;
+offset_x = 100;
 
 var data = [
-        {text: "onrust", value: -15.695442450570816},
-        {text: "uw_eigen_factor", value: -2.3778043292777675},
-        {text: "piekeren", value: 2.3736427124478197},
-        {text: "concentratie", value: 1.1899372553252616},
-        {text: "opgewektheid", value: -1.1078891632279635},
-        {text: "eigenwaarde", value: -0.876250480045563}
-    ];
+    {id: 1, name: "onrust", val: -15.695442450570816},
+    {id: 2, name: "uw_eigen_factor", val: -2.3778043292777675},
+    {id: 3, name: "piekeren", val: 2.3736427124478197},
+    {id: 4, name: "concentratie", val: 1.1899372553252616},
+    {id: 5, name: "opgewektheid", val: -1.1078891632279635},
+    {id: 6, name: "eigenwaarde", val: -0.876250480045563}
+];
+
+var links = [
+    {source: {"x": 0, "y": 5}, target: {"x": 0, "y": 45}},
+    {source: {"x": 0, "y": 5}, target: {"x": 0, "y": 90}},
+    {source: {"x": 0, "y": 5}, target: {"x": 0, "y": 135}},
+    {source: {"x": 0, "y": 135}, target: {"x": 0, "y": 90}},
+    {source: {"x": 0, "y": 180}, target: {"x": 0, "y": 225}}
+
+];
+
+
+var svgContainer = d3.select("#bubble_chart")
+    .style("height", height + "px")
+    .style("width", width + "px");
+
+var svg = d3.select("#bubble_chart").append("svg")
+    .attr("class", "bubble_chart_svg")
+    .attr("width", width)
+    .attr("height", height);
 
 var calculate_radius = function (value) {
     return Math.abs(value) * 2
 };
 
-var increaseSize = function (d, i) {
+var show_links = function (d, i) {
     id = "#bubble" + i;
+    svg.selectAll('.line')
+        .classed("hidden", false);
+
 };
 
-var decreaseSize = function (d, i) {
-    console.log(d + " leave! " + i);
+var hide_links = function (d, i) {
+    svg.selectAll('.line')
+        .classed("hidden", true);
 };
 
-var render = function(data) {
+var draw_arc = function(d, reverse) {
+    var dx = d.target.x - d.source.x,
+        dy = d.target.y - d.source.y,
+        dr = 75/2;
+    if(reverse)
+        return "M" + (offset_x+ d.target.x) + "," + (offset_y + d.target.y) + "A" + dr + "," + dr + " 0 0,1 " + (offset_x+ d.source.x) + "," + (offset_y+ d.source.y);
+    return "M" + (offset_x+ d.source.x) + "," + (offset_y + d.source.y) + "A" + dr + "," + dr + " 0 0,1 " + (offset_x+ d.target.x) + "," + (offset_y+ d.target.y);
+};
+
+var render = function (data) {
     console.log(data);
-    var svgContainer = d3.select("#bubble_chart")
-        .style("height", height + "px")
-        .style("width", width + "px");
-
-    var svg = d3.select("#bubble_chart").append("svg")
-        .attr("class", "bubble_chart_svg")
-        .attr("width", width)
-        .attr("height", height);
 
     svg.append("text")
         .attr("id", "bubbleItemNote")
@@ -52,6 +76,7 @@ var render = function(data) {
         .enter()
         .append("circle")
         .attr("class", "node")
+        .attr("opacity", "1.3")
         .attr("id", function (d, i) {
             console.log('i: ' + i);
             return "bubble" + i;
@@ -67,11 +92,22 @@ var render = function(data) {
         })
         .style("fill", "#3397da")
         .on("mouseleave", function (d, i) {
-            decreaseSize(d, i);
+            hide_links(d, i);
         })
         .on("mouseenter", function (d, i) {
-            increaseSize(d, i);
+            show_links(d, i);
         });
+
+    svg.selectAll("g")
+        .data(links)
+        .enter()
+        .append('path')
+        .attr("class", function(d, i) {return "hidden line"})
+        .attr("d", function(d){ return draw_arc(d, true) })
+        .style("stroke", "#ff0000")
+        .style("stroke-width", 2)
+        .attr("fill", "none");
+
 
     svg.selectAll('g')
         .data(data)
@@ -92,4 +128,6 @@ var render = function(data) {
         .attr("y", function (d, i) {
             return offset_y + 6 + i * 45 + "px"
         });
-}
+};
+
+render(data);
