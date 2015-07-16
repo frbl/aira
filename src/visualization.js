36814805a62a5bb213b2e7c30adadd02e3dbafe3
Frank Blaauw <frank.blaauw@gmail.com>
2015-07-16 13:22:15 +0200

    wip

--- src/visualization.js
+++ src/visualization.js
@@ -1,135 +1,136 @@
 function Visualization(node_names) {
-    this.node_names = node_names;
-    this.chart = {};
+  this.node_names = node_names;
+  this.chart = {};
 }
 
 Visualization.prototype.updateAdvice = function (effects) {
-    var result = $(".aira-result").find('.advice .content');
-    var number_of_measurements_per_day = 1;
-    var number_of_options = 0;
+  var result = $(".aira-result").find('.advice .content');
+  var number_of_measurements_per_day = 1;
+  var number_of_options = 0;
 
 
-    var html = '<ol>';
-    var i, current;
-    for (effect in effects) {
-        if (effects.hasOwnProperty(effect)) {
-            number_of_options += effects[effect].length;
+  var html = '<ol>';
+  var i, current;
+  for (var effect in effects) {
+    if (effects.hasOwnProperty(effect)) {
+      number_of_options += effects[effect].length;
 
-            for (i = 0; i < effects[effect].length; i++) {
-                current = effects[effect][i];
-                var length = current[1] - current[0];
-                var when = current[1] - (length);
+      for (i = 0; i < effects[effect].length; i++) {
+        current = effects[effect][i];
+        var length = current[1] - current[0];
+        var when = current[1] - (length);
 
-                html += ('<li>You could increase your level of ' + effect + ', every ' + (when / number_of_measurements_per_day).toFixed(1) + ' measurement moments. This has an effect that lasts for ' + (length / number_of_measurements_per_day).toFixed(2) + ' days </li>');
-            }
-        }
+        html += ('<li>You could increase your level of ' + effect + ', every ' + (when / number_of_measurements_per_day).toFixed(1) + ' measurement moments. This has an effect that lasts for ' + (length / number_of_measurements_per_day).toFixed(2) + ' days </li>');
+      }
     }
+  }
 
-    if (number_of_options > 0) {
-        result.html("<p>If you would like to increase the variable you just clicked, you can do " + convertNumberToText(number_of_options) + " thing" + (number_of_options == 1 ? '' : 's') + ":</p>");
-        result.append(html + '</ol>');
-    }
+  if (number_of_options > 0) {
+    result.html("<p>If you would like to increase the variable you just clicked, you can do " + convertNumberToText(number_of_options) + " thing" + (number_of_options == 1 ? '' : 's') + ":</p>");
+    result.append(html + '</ol>');
+  }
 };
 
 
 Visualization.prototype.updateNetEffect = function (effects, variable_id_to_improve) {
-    var number_of_options = 0;
+  var number_of_options = 0;
 
-    var result = $(".aira-result").find('.effect .content');
-    result.html('');
-    var current, text;
-    var html = '<ol>';
+  var result = $(".aira-result").find('.effect .content');
+  result.html('');
+  var current, text;
+  var html = '<ol>';
 
-    for (effect in effects) {
-        if (effects.hasOwnProperty(effect)) {
-            current = effects[effect].needed_difference * 100;
-            //if(current > 100) continue;
-            text = current > 0 ? 'increase' : 'decrease';
-            text = ('<li>You could '+text+' your average amount of ' + effect + ' with ' + Math.abs(current.toFixed(0)) + '%</li>');
+  for (var effect in effects) {
+    if (effects.hasOwnProperty(effect)) {
+      current = effects[effect].needed_difference * 100;
+      //if(current > 100) continue;
+      text = current > 0 ? 'increase' : 'decrease';
+      text = ('<li>You could '+text+' your average amount of ' + effect + ' with ' + Math.abs(current.toFixed(0)) + '%</li>');
 
-            number_of_options += 1;
-            html += text
+      number_of_options += 1;
+      html += text;
 
-        }
     }
-    result.append("<p>If you would like to change the '"+variable_mapping.get_value(variable_id_to_improve)+"'-variable with " + view_model.get_improvement() + "%, you can do " + convertNumberToText(number_of_options) + " thing" + (number_of_options == 1 ? '' : 's') + ":</p>");
-    result.append(html + '</ol>');
+  }
+  result.append("<p>If you would like to change the '"+variable_mapping.get_value(variable_id_to_improve)+"'-variable with " + view_model.get_improvement() + "%, you can do " + convertNumberToText(number_of_options) + " thing" + (number_of_options == 1 ? '' : 's') + ":</p>");
+  result.append(html + '</ol>');
 
 };
 
 
 Visualization.prototype._addPlotLine = function (x_value) {
-    this.chart.xAxis[0].addPlotLine({
-        value: x_value,
-        color: 'rgb(255, 0, 0)',
-        width: 1,
-        id: 'plot_line'
-    });
+  this.chart.xAxis[0].addPlotLine({
+    value: x_value,
+    color: 'rgb(255, 0, 0)',
+    width: 1,
+    id: 'plot_line'
+  });
 };
 
 Visualization.prototype._removePlotLine = function () {
-    this.chart.xAxis[0].removePlotLine('plot_line');
+  this.chart.xAxis[0].removePlotLine('plot_line');
 };
 
 Visualization.prototype.setPlotlineLocation = function (x_value) {
-    this._removePlotLine();
-    this._addPlotLine(x_value);
+  this._removePlotLine();
+  this._addPlotLine(x_value);
 };
 
 
 Visualization.prototype.addData = function (name, data) {
-    // find the clicked values and the series
-    this.chart.addSeries({
-        name: name,
-        data: data,
-        visible: false
-    });
+  // find the clicked values and the series
+  this.chart.addSeries({
+    name: name,
+    data: data,
+    visible: false
+  });
 };
 
 Visualization.prototype.draw = function (transposed_irf) {
-    var i;
-    var series_var = [];
-    for (i = 0; i < transposed_irf.length; i++) {
-        series_var.push({
-            name: this.node_names[i],
-            data: transposed_irf[i],
-            visible: true
-        })
-    }
-
-    this.chart = new Highcharts.Chart({
-        chart: {
-            renderTo: 'container'
-        },
-        plotOptions: {
-            series: {
-                marker: {
-                    enabled: false
-                }
-            }
-        },
-
-        title: {
-            text: 'Impulse response',
-            x: -20 //center
-        },
-        subtitle: {
-            text: 'Calculated using AIRA',
-            x: -20
-        },
-        credits: {
-            enabled: false
-        },
-        yAxis: {
-            title: {
-                text: 'Response'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-        series: series_var
+  var i;
+  var series_var = [];
+  for (i = 0; i < transposed_irf.length; i++) {
+    series_var.push({
+      name: this.node_names[i],
+      data: transposed_irf[i],
+      visible: true
     });
+  }
+
+  this.chart = new Highcharts.Chart({
+    chart: {
+      renderTo: 'container',
+      type: 'spline'
+    },
+    plotOptions: {
+      series: {
+        marker: {
+          enabled: false
+        }
+      }
+    },
+
+    title: {
+      text: 'Impulse response',
+      x: -20 //center
+    },
+    subtitle: {
+      text: 'Calculated using AIRA',
+      x: -20
+    },
+    credits: {
+      enabled: false
+    },
+    yAxis: {
+      title: {
+        text: 'Response'
+      },
+      plotLines: [{
+        value: 0,
+        width: 1,
+        color: '#808080'
+      }]
+    },
+    series: series_var
+  });
 };
