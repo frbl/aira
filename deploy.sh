a35bb8d99e098a21fe8ab9559b2604d8c90ea6d9
Frank Blaauw <frank.blaauw@gmail.com>
2015-08-25 16:43:33 +0200

    New version

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git co gh-pages
+cd ../impulse_response/wip
+cp layout/application.css ../../aira/stylesheets/
+cp one_network.json ../../aira/one_network.json
+compressjs src/*.js ../../aira/javascripts/output.min.js
+cd ../../aira
+git add --all
+git commit -m 'New version'
+git push
