e65ea36faf7bb36c768b2c85752df7843978195e
Frank Blaauw <frank.blaauw@gmail.com>
2015-08-27 16:14:13 +0200

    New version

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ cp layout/application.css ../../../../aira/stylesheets/
 cp one_network.json ../../../../aira/one_network.json
 compressjs src/*.js ../../../../aira/javascripts/aira.min.js
 cd ../../../../aira
+datestring=date
+echo "<!--$datestring-->" >> index.html
 git add --all
 git commit -m 'New version'
 git push
