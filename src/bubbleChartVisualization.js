bb57afb7026c0ff4be691ef9542e8693ecacc28a
Frank Blaauw <frank.blaauw@gmail.com>
2015-08-28 12:52:08 +0200

    Fixed network

--- src/bubbleChartVisualization.js
+++ src/bubbleChartVisualization.js
@@ -1,290 +1,291 @@
-var width = 400,
-  height = 400,
-  max_size = 40,
-  min_size = 10,
-  min = 999999,
-  max = -99999;
+var BubbleChartVisualization = (function() {
+  var width = 400,
+    height = 400,
+    max_size = 40,
+    min_size = 10,
+    min = 999999,
+    max = -99999;
 
-var bubble_force_layout = d3.layout.force()
-  .charge(-10000)
-  .linkDistance(100)
-  .linkStrength(linkStrength)
-  .friction(0.9)
-  .gravity(0.1)
-  .theta(0.1)
-  .size([width, height]);
+  var bubble_force_layout = d3.layout.force()
+    .charge(-10000)
+    .linkDistance(100)
+    .linkStrength(linkStrength)
+    .friction(0.9)
+    .gravity(0.1)
+    .theta(0.1)
+    .size([width, height]);
 
-var svg = d3.select("#bubble_chart").append("svg")
-  .attr("class", "bubble_chart_svg")
-  .attr("width", width)
-  .attr("height", height);
+  var svg = d3.select("#bubble_chart").append("svg")
+    .attr("class", "bubble_chart_svg")
+    .attr("width", width)
+    .attr("height", height);
 
-var determineMinMax = function() {
-  var val;
-  my_graph.nodes.forEach(function(node) {
-    val = Math.log(1 + Math.abs(node.val));
-    min = min < val ? min : val;
-    max = max > val ? max : val;
-  });
-};
-
-var get_radius = function(d) {
-  return ((Math.log(1 + Math.abs(d.val)) - min) / (max - min)) * (max_size - min_size) + min_size;
-};
-
-var get_direction = function(d) {
-  var res = 'Meer ';
-  if (Math.sign(d.val) == -1) res = 'Minder ';
-  return res + d.name.toLowerCase();
-};
-
-var removeAllElements = function(d) {
-  d.selectAll("*").remove();
-  min = 999999;
-  max = -99999;
-};
-
-var resetNodes = function() {
-  svg.selectAll('line').classed("visible-line-click", false);
-  svg.selectAll('line').classed("visible-line-hover", false);
-
-  svg.selectAll('.node')
-    .style('stroke-width', '0px')
-    .style('stroke', '#fff')
-    .classed('selected_node', false);
-};
-
-var resetNodeNames = function(d, original) {
-  if (d === undefined) d = {
-    "index": -1
+  var determineMinMax = function() {
+    var val;
+    my_graph.nodes.forEach(function(node) {
+      val = Math.log(1 + Math.abs(node.val));
+      min = min < val ? min : val;
+      max = max > val ? max : val;
+    });
   };
-  my_graph.nodes.forEach(function(node) {
-    svg.select("#label_" + node.key).text(node.index == d.index || original ? get_direction(node) : node.name);
-  });
-};
 
-var renderResultText = function(text) {
-  var result = "";
+  var get_radius = function(d) {
+    return ((Math.log(1 + Math.abs(d.val)) - min) / (max - min)) * (max_size - min_size) + min_size;
+  };
 
-  if (text.length == 1) {
-    result = "Wanneer u " + text[0] + " ervaart, wordt dat gevolgd door meer leefplezier, maar heeft dat geen significant effect op de andere variabelen.";
-  } else {
-    result += "Wanneer u " + text[0] + " ervaart, wordt dat gevolgd door ";
-    for (var i = 1; i < text.length; i++) {
-      if (text.length == 2) {
-        result += text[i] + ".";
-      } else if (i == text.length - 1) {
-        result += "en " + text[i] + ".";
-      } else {
-        result += text[i] + ", ";
+  var get_direction = function(d) {
+    var res = 'Meer ';
+    if (Math.sign(d.val) == -1) res = 'Minder ';
+    return res + d.name.toLowerCase();
+  };
+
+  var removeAllElements = function(d) {
+    d.selectAll("*").remove();
+    min = 999999;
+    max = -99999;
+  };
+
+  var resetNodes = function() {
+    svg.selectAll('line').classed("visible-line-click", false);
+    svg.selectAll('line').classed("visible-line-hover", false);
+
+    svg.selectAll('.node')
+      .style('stroke-width', '0px')
+      .style('stroke', '#fff')
+      .classed('selected_node', false);
+  };
+
+  var resetNodeNames = function(d, original) {
+    if (d === undefined) d = {
+      "index": -1
+    };
+    my_graph.nodes.forEach(function(node) {
+      svg.select("#label_" + node.key).text(node.index == d.index || original ? get_direction(node) : node.name);
+    });
+  };
+
+  var renderResultText = function(text) {
+    var result = "";
+
+    if (text.length == 1) {
+      result = "Wanneer u " + text[0] + " ervaart, wordt dat gevolgd door meer leefplezier, maar heeft dat geen significant effect op de andere variabelen.";
+    } else {
+      result += "Wanneer u " + text[0] + " ervaart, wordt dat gevolgd door ";
+      for (var i = 1; i < text.length; i++) {
+        if (text.length == 2) {
+          result += text[i] + ".";
+        } else if (i == text.length - 1) {
+          result += "en " + text[i] + ".";
+        } else {
+          result += text[i] + ", ";
+        }
       }
     }
+    d3.select("#effect_list").select("span").append("span").text(result);
+  };
+
+  var renderNodesFromNodePerspective = function(d) {
+    var result_text = [];
+    result_text.push(get_direction(d).toLowerCase());
+    my_graph.links.forEach(function(edge) {
+      if (edge.source.index == d.index) {
+        var res = my_graph.nodes.filter(function(node) {
+          return node.index == edge.target.index;
+        }).map(function(node) {
+          return {
+            "val": Math.sign(edge.weight * d.val),
+            "key": node.key,
+            "name": node.name
+          };
+        })[0];
+        svg.select("#label_" + res.key).text(function(d) {
+          return get_direction(res);
+        });
+        result_text.push(get_direction(res).toLowerCase());
+      }
+    });
+    renderResultText(result_text);
+  };
+
+  var renderEdgesFromNodePerspective = function(d) {
+    svg.selectAll('line').filter(function(line) {
+      return line.source.index === d.index;
+    }).classed("visible-line-click", function(d) {
+      return !d3.select(this).classed("visible-line-click");
+    });
+  };
+
+
+  var onNodeClick = function(d) {
+    var clicked_node = d3.select(this);
+    var previously_clicked = clicked_node.classed('selected_node');
+
+    removeAllElements(d3.select("#effect_list").select("span"));
+
+    resetNodes();
+    resetNodeNames(d, previously_clicked);
+    if (previously_clicked) return;
+
+    clicked_node.style('stroke-width', '2px')
+      .style('stroke', '#ff0000')
+      .classed('selected_node', true);
+
+    renderEdgesFromNodePerspective(d);
+    renderNodesFromNodePerspective(d);
+  };
+
+  function linkStrength(d) {
+    return d.distance;
   }
-  d3.select("#effect_list").select("span").append("span").text(result);
-};
 
-var renderNodesFromNodePerspective = function(d) {
-  var result_text = [];
-  result_text.push(get_direction(d).toLowerCase());
-  my_graph.links.forEach(function(edge) {
-    if (edge.source.index == d.index) {
-      var res = my_graph.nodes.filter(function(node) {
-        return node.index == edge.target.index;
-      }).map(function(node) {
-        return {
-          "val": Math.sign(edge.weight * d.val),
-          "key": node.key,
-          "name": node.name
-        };
-      })[0];
-      svg.select("#label_" + res.key).text(function(d) {
-        return get_direction(res);
+  var onNodeOut = function(d) {
+    svg.selectAll('line').classed("visible-line-hover", false);
+  };
+
+  var onNodeEnter = function(d) {
+    svg.selectAll('line').filter(function(line) {
+      return line.source.index === d.index;
+    }).classed("visible-line-hover", true);
+  };
+
+  var findNode = function(id) {
+    var x = my_graph.nodes.filter(function(node) {
+      return node.index === id;
+    });
+    return x[0];
+  };
+
+  function BubbleChartVisualization() {}
+
+  BubbleChartVisualization.prototype.render = function(graph_from_json) {
+    removeAllElements(svg);
+    my_graph = graph_from_json;
+
+    determineMinMax();
+
+    bubble_force_layout.nodes(my_graph.nodes)
+      .links(my_graph.links);
+    svg.append("svg:defs").selectAll("marker")
+      .data(my_graph.links)
+      .enter().append("svg:marker")
+      .attr("id", function(d) {
+        return "arrow-head-" + d.source.index + "-" + d.target.index;
+      })
+      .attr("viewBox", "0 -5 10 10")
+      .attr("refX", function(d) {
+        return get_radius(findNode(d.target.index)) * (6 / 10) / Math.sqrt(2) + 10;
+      })
+      .attr("refY", 0)
+      .attr("markerWidth", 6)
+      .attr("markerHeight", 6)
+      .attr("orient", "auto")
+      .attr("fill", "#666")
+      .append("svg:path")
+      .attr("d", "M0,-5L10,0L0,5");
+
+    var link = svg.selectAll(".link")
+      .data(my_graph.links)
+      .enter().append("line")
+      .attr("class", "line")
+      .style("stroke", "#B8CEB3")
+      .attr("fill", "none")
+      .style("stroke-width", function(d) {
+        return 4;
+      })
+      .attr("marker-end", function(d) {
+        return "url(#arrow-head-" + d.source.index + "-" + d.target.index + ")";
       });
-      result_text.push(get_direction(res).toLowerCase());
-    }
-  });
-  renderResultText(result_text);
-};
 
-var renderEdgesFromNodePerspective = function(d) {
-  svg.selectAll('line').filter(function(line) {
-    return line.source.index === d.index;
-  }).classed("visible-line-click", function(d) {
-    return !d3.select(this).classed("visible-line-click");
-  });
-};
+    var node = svg.selectAll(".node")
+      .data(my_graph.nodes)
+      .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", get_radius)
+      .style("fill", "#3397da")
+      .on({
+        "mouseenter": onNodeEnter,
+        "mouseout": onNodeOut,
+        "click": onNodeClick
+      });
+
+    var text = svg.selectAll(".text")
+      .data(my_graph.nodes)
+      .enter().append("text")
+      .attr("x", 8)
+      .attr("y", ".41em")
+      .attr("id", function(d) {
+        return "label_" + d.key;
+      })
+      .style("text-anchor", "middle")
+      .text(function(d) {
+        return get_direction(d);
+      });
 
 
-var onNodeClick = function(d) {
-  var clicked_node = d3.select(this);
-  var previously_clicked = clicked_node.classed('selected_node');
+    bubble_force_layout.start();
+    var minx,
+      miny,
+      maxx,
+      maxy;
 
-  removeAllElements(d3.select("#effect_list").select("span"));
 
-  resetNodes();
-  resetNodeNames(d, previously_clicked);
-  if (previously_clicked) return;
+    bubble_force_layout.on("tick", function() {
+      link.attr("x1", function(d) {
+          return d.source.x;
+        })
+        .attr("y1", function(d) {
+          return d.source.y;
+        })
+        .attr("x2", function(d) {
+          return d.target.x;
+        })
+        .attr("y2", function(d) {
+          return d.target.y;
+        })
+        .style("stroke", "#999");
 
-  clicked_node.style('stroke-width', '2px')
-    .style('stroke', '#ff0000')
-    .classed('selected_node', true);
+      minx = 10000;
+      miny = 10000;
+      maxx = -10000;
+      maxy = -10000;
+      my_graph.nodes.forEach(function(node) {
+        if (node.x < minx) minx = node.x;
+        if (node.x > maxx) maxx = node.x;
+        if (node.y < miny) miny = node.y;
+        if (node.y > maxy) maxy = node.y;
+      });
 
-  renderEdgesFromNodePerspective(d);
-  renderNodesFromNodePerspective(d);
-};
+      my_graph.nodes.forEach(function(node) {
+        if (maxx - minx > 0.05) node.x = 100 + (width - 200) * (node.x - minx) / (maxx - minx);
+        if (maxy - miny > 0.05) node.y = 100 + (height - 200) * (node.y - miny) / (maxy - miny);
+      });
 
-var linkStrength = function(d) {
-  return d.distance;
-};
+      node.attr("cx", function(d) {
+          d.x = Math.max(get_radius(d), Math.min(width - get_radius(d), d.x));
+          return d.x;
+        })
+        .attr("cy", function(d) {
+          d.y = Math.max(get_radius(d), Math.min(height - get_radius(d), d.y));
+          return d.y;
+        });
 
-var onNodeOut = function(d) {
-  svg.selectAll('line').classed("visible-line-hover", false);
-};
+      //.attr("y", function(d){return d.y+ get_radius(d) + 10;});
 
-var onNodeEnter = function(d) {
-  svg.selectAll('line').filter(function(line) {
-    return line.source.index === d.index;
-  }).classed("visible-line-hover", true);
-};
+      var x_offset = 20;
+      var y_offset = 3;
 
-var findNode = function(id) {
-  var x = my_graph.nodes.filter(function(node) {
-    return node.index === id;
-  });
-  return x[0];
-};
-
-function BubbleChartVisualization() {}
-
-render = function(graph_from_json) {
-  console.log(graph_from_json);
-  removeAllElements(svg);
-  my_graph = graph_from_json;
-
-  determineMinMax();
-
-  bubble_force_layout.nodes(my_graph.nodes)
-    .links(my_graph.links);
-  //svg.append("svg:defs").selectAll("marker")
-    //.data(my_graph.links)
-    //.enter().append("svg:marker")
-    //.attr("id", function(d) {
-      //return "arrow-head-" + d.source.index + "-" + d.target.index;
-    //})
-    //.attr("viewBox", "0 -5 10 10")
-    //.attr("refX", function(d) {
-      //return get_radius(findNode(d.target.index)) * (6 / 10) / Math.sqrt(2) + 10;
-    //})
-    //.attr("refY", 0)
-    //.attr("markerWidth", 6)
-    //.attr("markerHeight", 6)
-    //.attr("orient", "auto")
-    //.attr("fill", "#666")
-    //.append("svg:path")
-    //.attr("d", "M0,-5L10,0L0,5");
-
-  //var link = svg.selectAll(".link")
-    //.data(my_graph.links)
-    //.enter().append("line")
-    //.attr("class", "line")
-    //.style("stroke", "#B8CEB3")
-    //.attr("fill", "none")
-    //.style("stroke-width", function(d) {
-      //return 4;
-    //})
-    //.attr("marker-end", function(d) {
-      //return "url(#arrow-head-" + d.source.index + "-" + d.target.index + ")";
-    //});
-
-  //var node = svg.selectAll(".node")
-    //.data(my_graph.nodes)
-    //.enter().append("circle")
-    //.attr("class", "node")
-    //.attr("r", get_radius)
-    //.style("fill", "#3397da")
-    //.on({
-      //"mouseenter": onNodeEnter,
-      //"mouseout": onNodeOut,
-      //"click": onNodeClick
-    //});
-
-  //var text = svg.selectAll(".text")
-    //.data(my_graph.nodes)
-    //.enter().append("text")
-    //.attr("x", 8)
-    //.attr("y", ".41em")
-    //.attr("id", function(d) {
-      //return "label_" + d.key;
-    //})
-    //.style("text-anchor", "middle")
-    //.text(function(d) {
-      //return get_direction(d);
-    //});
-
-console.log(my_graph);
-fail()
-
-  bubble_force_layout.start();
-  var minx,
-    miny,
-    maxx,
-    maxy;
-
-  bubble_force_layout.on("tick", function() {
-    //link.attr("x1", function(d) {
-        //return d.source.x;
-      //})
-      //.attr("y1", function(d) {
-        //return d.source.y;
-      //})
-      //.attr("x2", function(d) {
-        //return d.target.x;
-      //})
-      //.attr("y2", function(d) {
-        //return d.target.y;
-      //})
-      //.style("stroke", "#999");
-
-    minx = 10000;
-    miny = 10000;
-    maxx = -10000;
-    maxy = -10000;
-    my_graph.nodes.forEach(function(node) {
-      if (node.x < minx) minx = node.x;
-      if (node.x > maxx) maxx = node.x;
-      if (node.y < miny) miny = node.y;
-      if (node.y > maxy) maxy = node.y;
+      text.attr("x", function(d) {
+          return d.x;
+        })
+        //text.attr("x", function(d) { return d.x > width / 2 ? d.x + get_radius(d) + x_offset : d.x - get_radius(d) - x_offset ;})
+        .attr("y", function(d) {
+          return d.y > height / 2 ? d.y + get_radius(d) + x_offset : d.y - get_radius(d) - y_offset;
+        });
     });
 
-    my_graph.nodes.forEach(function(node) {
-      if (maxx - minx > 0.05) node.x = 100 + (width - 200) * (node.x - minx) / (maxx - minx);
-      if (maxy - miny > 0.05) node.y = 100 + (height - 200) * (node.y - miny) / (maxy - miny);
-    });
+    for (var i = 0; i < 400; i++) bubble_force_layout.tick();
 
-    //node.attr("cx", function(d) {
-        //d.x = Math.max(get_radius(d), Math.min(width - get_radius(d), d.x));
-        //return d.x;
-      //})
-      //.attr("cy", function(d) {
-        //d.y = Math.max(get_radius(d), Math.min(height - get_radius(d), d.y));
-        //return d.y;
-      //});
+  };
 
-    //.attr("y", function(d){return d.y+ get_radius(d) + 10;});
-
-    var x_offset = 20;
-    var y_offset = 3;
-
-    //text.attr("x", function(d) {
-        //return d.x;
-      //})
-      ////text.attr("x", function(d) { return d.x > width / 2 ? d.x + get_radius(d) + x_offset : d.x - get_radius(d) - x_offset ;})
-      //.attr("y", function(d) {
-        //return d.y > height / 2 ? d.y + get_radius(d) + x_offset : d.y - get_radius(d) - y_offset;
-      //});
-
-  });
-
-  //for (var i = 0; i < 400; i++) bubble_force_layout.tick();
-
-};
+  return BubbleChartVisualization;
+})();
--- src/javascript.js
+++ src/javascript.js
@@ -8,7 +8,10 @@ var injectButtons = function(node_names) {
     var html = $('<input class="button light button_' + node + '" name="' + node + '" type="button" value="' + variable_mapping.get_value(node) + '" id="button_' + node + '"/>');
 
     location.append(html);
-    location.on("click", ".button_" + node, {current_id: i, current_node: node}, function(event){
+    location.on("click", ".button_" + node, {
+      current_id: i,
+      current_node: node
+    }, function(event) {
       var name = event.data.current_node;
       var myid = event.data.current_id;
       clickNode(name, myid);
@@ -16,69 +19,69 @@ var injectButtons = function(node_names) {
   }
 
   location.append($('<input class="button red button_all" name="button_all" type="button" value="Shock ALL!" id="button_button_all"/>'));
-  location.on("click", ".button_all", function () {
+  location.on("click", ".button_all", function() {
     clickNode('All', -1);
   });
 
   location.append($('<input class="button green button_reset" name="button_reset" type="button" value="Reset" id="button_button_reset"/>'));
-  location.on("click", ".button_reset", function () {
+  location.on("click", ".button_reset", function() {
     simulation.clear();
   });
 
   location.append($('<input class="button dark button_optimize" name="button_optimize" type="button" value="Find optimal node" id="button_button_reset"/>'));
-  location.on("click", ".button_optimize", function () {
+  location.on("click", ".button_optimize", function() {
     var res = aira.determineBestNodeFromAll();
     var html = '<ol>';
-    for( var i = 0 ; i < res.length ; i++ ) {
-      html = html + '<li><strong>'+ res[i].name + '</strong>: '+ res[i].val + '</li>';
+    for (var i = 0; i < res.length; i++) {
+      html = html + '<li><strong>' + res[i].name + '</strong>: ' + res[i].val + '</li>';
     }
     html += '</ol>';
     $(".effect .content").html(html);
+    var bubbleChartVisualization = new BubbleChartVisualization();
     res = aira.createAiraNetworkJson(res);
-    render((res));
+    bubbleChartVisualization.render(res);
   });
 };
 
-
-var injectSimulationFunctionality = function () {
+var injectSimulationFunctionality = function() {
   var location = $('#simulation-buttons');
 
-  location.on("click", ".button_pause", function () {
+  location.on("click", ".button_pause", function() {
     simulation.pause();
   });
-  location.on("click", ".button_play", function () {
+  location.on("click", ".button_play", function() {
     simulation.run();
   });
-  location.on("click", ".button_next", function () {
+  location.on("click", ".button_next", function() {
     simulation.simulateStep(1);
   });
-  location.on("click", ".button_previous", function () {
+  location.on("click", ".button_previous", function() {
     simulation.simulateStep(-1);
   });
 };
 
 
-var generateSelectOptions = function (from, to, stepsize, location) {
+var generateSelectOptions = function(from, to, stepsize, location) {
   html = $(location);
   for (i = from; i < to; i += stepsize) {
     html.append($("<option></option>")
-                .attr("value", i)
-                .text(roundToPlaces(i, 2)));
+      .attr("value", i)
+      .text(roundToPlaces(i, 2)));
   }
   return html;
 };
 
-var appendSelectOptions = function (data, location) {
+var appendSelectOptions = function(data, location) {
   html = $(location);
   for (i = 0; i < data.length; i++) {
     html.append($("<option></option>")
-                .attr("value", data[i])
-                .text(data[i]));
+      .attr("value", data[i])
+      .text(data[i]));
   }
   return html;
 };
 
-var convertNumberToText = function (number) {
+var convertNumberToText = function(number) {
   if (number < 1) return 'very few';
   if (number > 20) return 'alot of';
   return {
@@ -105,7 +108,7 @@ var convertNumberToText = function (number) {
 };
 
 
-var clickNode = function (node_name_id, node_id) {
+var clickNode = function(node_name_id, node_id) {
   if (DEBUG >= 1) console.log('Impulse given on: ' + node_name_id + ' (' + node_id + ')');
 
   console.log(node_id + " < id and name > " + node_name_id);
@@ -120,8 +123,12 @@ var clickNode = function (node_name_id, node_id) {
   simulation.run(true);
 
   if (node_id != -1) {
-    var thresholdOptimizer = new ThresholdOptimizer({threshold: view_model.get_threshold()});
-    var netEffectOptimizer = new NetEffectOptimizer({wanted_increase: view_model.get_improvement()});
+    var thresholdOptimizer = new ThresholdOptimizer({
+      threshold: view_model.get_threshold()
+    });
+    var netEffectOptimizer = new NetEffectOptimizer({
+      wanted_increase: view_model.get_improvement()
+    });
 
     if ($('#chk-threshold').prop('checked'))
       visualization_engine.updateAdvice(aira.determineOptimalNodeSimple(node_id, thresholdOptimizer));
@@ -138,4 +145,3 @@ var clickNode = function (node_name_id, node_id) {
   }
 
 };
-
