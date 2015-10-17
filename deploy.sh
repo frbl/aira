e606d9a42935a9a0df2c88031ee7222eb2c04648
Frank Blaauw <frank.blaauw@gmail.com>
2015-10-17 10:16:15 +0200

    Updated deploy, added gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+git co master
+cp layout/application.css ../application.css
+cp one_network.json ../one_network.json
+compressjs src/*.js ../aira.min.js
 git co gh-pages
-cd ../papers/2015/impulse_response/wip
-cp layout/application.css ../../../../aira/stylesheets/
-cp one_network.json ../../../../aira/one_network.json
-compressjs src/*.js ../../../../aira/javascripts/aira.min.js
-cd ../../../../aira
+mv ../application.css stylesheets/application.css
+mv ../one_network.json one_network.json
+mv ../aira.min.js javascripts/aira.min.js
+
 sed '$ d' index.html> index.html.tmp
 mv index.html.tmp index.html
 datestring=`date`
