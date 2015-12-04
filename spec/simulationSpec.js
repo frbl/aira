3be01e464ad521ed8dc8af3a8ad407fd984e9e27
Frank Blaauw <frank.blaauw@gmail.com>
2015-12-04 14:44:39 +0100

    Modularized simulation, added test placeholder

---
+++ spec/simulationSpec.js
@@ -0,0 +1,34 @@
+describe("Simulation", function () {
+
+    describe("run", function () {
+
+    });
+
+    describe("setIrf", function () {
+
+    });
+
+    describe("setStepsToRun", function () {
+
+    });
+
+    describe("simulateStep", function () {
+
+    });
+
+    describe("resetNodes", function () {
+
+    });
+
+    describe("pause", function () {
+
+    });
+
+    describe("plotValue", function () {
+
+    });
+
+    describe("clear", function () {
+
+    });
+});
