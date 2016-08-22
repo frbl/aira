8f887971745988d8d99d942c5b76c7c49bb29f26
Frank Blaauw <frank.blaauw@gmail.com>
2016-08-22 18:56:40 +0200

    Added some scss code

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,36 @@
 var gulp = require('gulp');
+
+// Compiles scss to css
 var sass = require('gulp-sass');
+
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
 
+/**
+ * Task to compile scss to sass
+ */
 gulp.task('sass', function() {
-  console.log('called');
-  return gulp.src('app/scss/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('app/styles'))
-    .pipe(browserSync.stream());
+	return gulp.src('app/scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('app/styles'))
+		.pipe(browserSync.stream());
 });
 
+/**
+ * Task to run the application
+ */
 gulp.task('serve', ['sass'], function() {
-  browserSync({
-    server: './app'
-  });
+	browserSync({
+		server: './app'
+	});
 
-  gulp.watch('app/scss/**/*.scss', ['sass']);
-  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {
-    cwd: 'app'
-  }, reload);
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {
+		cwd: 'app'
+	}, reload);
 });
 
 gulp.task('default', ['serve'], function() {
-  // implement
+	// implement
 });
