03180b3d2763a9d61615f78ebb1c2aaeee92ee3a
Frank Blaauw <frank.blaauw@gmail.com>
2016-08-22 18:32:51 +0200

    Implement SCSS with live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,28 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
-gulp.task('serve', function() {
+
+gulp.task('sass', function() {
+  console.log('called');
+  return gulp.src('app/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/styles'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass'], function() {
   browserSync({
-    server: {
-      baseDir: 'app'
-    }
+    server: './app'
   });
+
+  gulp.watch('app/scss/**/*.scss', ['sass']);
   gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {
     cwd: 'app'
   }, reload);
 });
 
-
-gulp.task('default', function() {
+gulp.task('default', ['serve'], function() {
   // implement
 });
